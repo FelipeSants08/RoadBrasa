0fd517f8a5a41b43b3bdd803df60dbf6279e58e4
FelipeSants08 <felipe_santana.01@hotmail.com>
2025-04-03 09:08:40 -0300

    Criado um get, e adicionando o repository

--- road-brasa-api/road-brasa-api/src/main/java/com/br/road_brasa_api/controller/ViagemController.java
+++ road-brasa-api/road-brasa-api/src/main/java/com/br/road_brasa_api/controller/ViagemController.java
@@ -1,5 +1,28 @@
 package com.br.road_brasa_api.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.road_brasa_api.model.Viagem;
+import com.br.road_brasa_api.repository.ViagemRepository;
+
+@RestController
+@RequestMapping("/viagem")
 public class ViagemController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private ViagemRepository repository;
+
+    public List<Viagem> index() {
+        log.info("Busacando Viagem");
+        return repository.findAll();
+    }
 
 }
---
+++ road-brasa-api/road-brasa-api/src/main/java/com/br/road_brasa_api/repository/ViagemRepository.java
@@ -0,0 +1,9 @@
+package com.br.road_brasa_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.road_brasa_api.model.Viagem;
+
+public interface ViagemRepository extends JpaRepository<Viagem, Long> {
+
+}
